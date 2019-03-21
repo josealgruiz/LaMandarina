6360fde4ef979b24ad521400aa2fe6c8f55970bd
Ricardo Gomez <ricardogb0902@gmail.com>
2019-03-21 00:20:49 -0400

    Crud Productos y algo del home

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { map } from "rxjs/operators";
-<<<<<<< HEAD
 import { auth } from "firebase/app";
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { userInterface } from "../models/user";
 import { Observable } from 'rxjs';
-=======
-import { auth, User } from "firebase/app";
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection} from '@angular/fire/firestore';
-import { userInterface } from 'src/app/models/user';
-import { Observable } from 'rxjs';
-
-
-
->>>>>>> origin/DevJose
 
 
 @Injectable({
@@ -24,27 +14,7 @@ export class AuthService {
 
   userCollection: AngularFirestoreCollection<userInterface>;
   users: Observable<userInterface[]>;
-<<<<<<< HEAD
   userDoc: AngularFirestoreDocument<userInterface>; 
-/* productsCollection: AngularFirestoreCollection<Product>;
-  products: Observable<Product[]>;
-  productDoc: AngularFirestoreDocument<Product>;
-
-  constructor(public db: AngularFirestore) { 
-    this.productsCollection = this.db.collection('products');
-    this.products = this.productsCollection.snapshotChanges().pipe(map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as Product;
-          data.id = a.payload.doc.id;
-          return data;
-        });
-      }));
-  }*/
-  constructor(private afsAuth: AngularFireAuth, private afs: AngularFirestore) { }
-=======
-  user: Observable<userInterface>;
-  userDoc: AngularFirestoreDocument<userInterface>; 
-
   constructor(private afsAuth: AngularFireAuth, private afs: AngularFirestore, public db: AngularFirestore) { 
     this.userCollection = this.db.collection('users');
     this.users = this.userCollection.snapshotChanges().pipe(map(actions => {
@@ -55,9 +25,6 @@ export class AuthService {
       });
     }));
   }
-
->>>>>>> origin/DevJose
-
   registerUser(email: string, pass: string) {
     return new Promise((resolve, reject) => {
       this.afsAuth.auth.createUserWithEmailAndPassword(email, pass)
@@ -94,7 +61,6 @@ export class AuthService {
     const data: userInterface = {
       id: user.uid,
       email: user.email,
-      active: true,
       roles: {
         admin: true
       }
@@ -104,31 +70,8 @@ export class AuthService {
 
 
   isUserAdmin(userUid) {
-    return this.afs.doc<userInterface>(`users/${userUid}`);
+    return this.afs.doc<userInterface>(`users/${userUid}`).valueChanges();
   }
 
-getProfile(user : userInterface){
-  const usuario = this.afs.doc<userInterface>(`users/${user.active}`);
-  return user.active;
-}
-
-/*
-signInWithEmailAndPassword(email: string, password: string) {
-  return this.afsAuth.auth.signInWithEmailAndPassword(email, password)
-    .then((credential) => {
-      this.userInterface.getUser(credential.user.uid).subscribe(user => {
-        if (user.isActive === true) {
-          console.log('activo mano')
-          this.userService.setUser(credential.user.uid);
-          this.router.navigate(['/home']);
-        } else {
-          console.log('estas pegao\'')
-          this.getOut();
-          this.router.navigate(['/userdisabled']);
-        }
-      })
-    })
-}
-*/
 
 }
