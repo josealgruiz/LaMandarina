0da564df404966c8fff3dd521ee39b946802ff8c
Ricardo Gomez <ricardogb0902@gmail.com>
2019-04-03 20:32:54 -0400

    Shopping cart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,12 +3,14 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Product } from "../models/products";
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
+import { userInterface } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private carrito: Observable<Product[]>;
   productsCollection: AngularFirestoreCollection<Product>;
   products: Observable<Product[]>;
   productDoc: AngularFirestoreDocument<Product>;
@@ -16,11 +18,58 @@ export class ProductService {
   public selectedProduct: Product = {
     id: null
   };
+  private idUser:string='';
+  private WishList: Observable<Product[]>;
+  
 
   constructor(public db: AngularFirestore) { 
     this.productsCollection = db.collection<Product>('products');
     this.products = this.productsCollection.valueChanges();
+    this.carrito = this.db.doc(this.idUser).collection<Product>('carrito').valueChanges();
   }
+
+  mandarCarrito(idProduct:any){
+    var messageRef = this.db.doc(this.idUser).collection('carrito').add(idProduct);
+  }
+  mandarWishList(idProduct:any){
+    var messageRef = this.db.doc(this.idUser).collection('deseos').add(idProduct);
+  }
+
+  getCarrito(){
+  
+    return this.carrito = this.db.doc(this.idUser).collection('carrito').snapshotChanges()
+    .pipe(map(changes=>{
+      return changes.map(action =>{
+        const data = action.payload.doc.data() as Product;
+        data.id = action.payload.doc.id;
+        return data;
+      })
+  
+    }));
+  }
+  getWishList(){
+    
+    return this.WishList = this.db.doc(this.idUser).collection('deseos').snapshotChanges()
+    .pipe(map(changes=>{
+      return changes.map(action =>{
+        const data = action.payload.doc.data() as Product;
+        data.id = action.payload.doc.id;
+        return data;
+      })
+  
+    }));
+  }
+  deleteProductCarrito(idProduct: string): void{
+    this.productDoc = this.db.collection<userInterface>('users').doc(this.idUser).collection('carrito').doc(idProduct);
+    this.productDoc.delete();
+  }
+  
+  deleteProductDeseos(idProduct: string): void{
+    this.productDoc = this.db.collection<userInterface>('users').doc(this.idUser).collection('deseos').doc(idProduct);
+    this.productDoc.delete();
+  }
+
+  
 
   getProducts(){
     return this.products = this.productsCollection.snapshotChanges()
